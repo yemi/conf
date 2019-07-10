4fda9d1a4e57d3653fb32d4a19e3888a170ccb5c
Fabian Rios <fabian.rios@rhyme.se>
2019-07-10 17:14:57 +0200

    Minor update

--- omf/init.fish
+++ omf/init.fish
@@ -2,10 +2,14 @@ set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 # Global
-set -gx EDITOR /usr/local/bin/emacs
+set -gx EDITOR /usr/bin/vim
 
 # Stack
 set PATH "$HOME/.local/bin" $PATH
 
 # Abbreviations
 abbr -a dc docker-compose
+abbr -a gp git pull
+
+# Android
+set PATH "$HOME/Library/Android/sdk/platform-tools/" $PATH
