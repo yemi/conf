b3fde6ff5a0515843c24cd38e0e809648a720d14
yemi <fabian@meetball.live>
2021-12-14 04:30:37 +0100

    add gitignore

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,14 @@ bass source /etc/profile
 
 # Use if flatpak doesn't work:
 # See: https://github.com/fish-shell/fish-shell/issues/7485#issuecomment-728984689
-#set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
-#set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 
-#for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
-#    if test -d $flatpakdir
-#        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
-#    end
-#end
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
